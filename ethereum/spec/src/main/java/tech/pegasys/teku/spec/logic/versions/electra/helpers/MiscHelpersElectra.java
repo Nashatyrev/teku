12030541306434cb9e47d9041078521b4292ffce
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2024-04-18 21:38:54 +0400

    Add computeSubnetForDataColumnSidecar() to the spec

--- ethereum/spec/src/main/java/tech/pegasys/teku/spec/logic/versions/electra/helpers/MiscHelpersElectra.java
+++ ethereum/spec/src/main/java/tech/pegasys/teku/spec/logic/versions/electra/helpers/MiscHelpersElectra.java
@@ -18,21 +18,39 @@ import tech.pegasys.teku.infrastructure.unsigned.UInt64;
 import tech.pegasys.teku.spec.config.SpecConfigElectra;
 import tech.pegasys.teku.spec.datastructures.state.beaconstate.BeaconState;
 import tech.pegasys.teku.spec.datastructures.state.beaconstate.versions.electra.BeaconStateElectra;
+import tech.pegasys.teku.spec.logic.common.helpers.MiscHelpers;
 import tech.pegasys.teku.spec.logic.common.helpers.Predicates;
 import tech.pegasys.teku.spec.logic.versions.deneb.helpers.MiscHelpersDeneb;
 import tech.pegasys.teku.spec.schemas.SchemaDefinitionsElectra;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class MiscHelpersElectra extends MiscHelpersDeneb {
+
+  public static MiscHelpersElectra required(final MiscHelpers miscHelpers) {
+    return miscHelpers
+        .toVersionElectra()
+        .orElseThrow(
+            () ->
+                new IllegalArgumentException(
+                    "Expected Electra misc helpers but got: "
+                        + miscHelpers.getClass().getSimpleName()));
+  }
+
+  private final SpecConfigElectra specConfigElectra;
 
   public MiscHelpersElectra(
       final SpecConfigElectra specConfig,
       final Predicates predicates,
       final SchemaDefinitionsElectra schemaDefinitions) {
     super(specConfig, predicates, schemaDefinitions);
+    this.specConfigElectra = specConfig;
   }
 
   @Override
@@ -43,6 +61,18 @@ public class MiscHelpersElectra extends MiscHelpersDeneb {
     return state
         .getEth1DepositIndex()
         .equals(BeaconStateElectra.required(state).getDepositReceiptsStartIndex());
+  }
+
+  public UInt64 computeSubnetForDataColumnSidecar(UInt64 columnIndex) {
+    return columnIndex.mod(specConfigElectra.getDataColumnSidecarSubnetCount());
+  }
+
+  public Set<UInt64> computeCustodyColumnIndexes(final UInt256 nodeId, final UInt64 epoch, final int subnetCount) {
+    Set<UInt64> subnets = new HashSet<>(computeDataColumnSidecarBackboneSubnets(nodeId, epoch, subnetCount));
+    return Stream.iterate(UInt64.ZERO, UInt64::increment)
+        .limit(specConfigElectra.getNumberOfColumns().intValue())
+        .filter(columnIndex -> subnets.contains(computeSubnetForDataColumnSidecar(columnIndex)))
+        .collect(Collectors.toSet());
   }
 
   public List<UInt64> computeDataColumnSidecarBackboneSubnets(final UInt256 nodeId, final UInt64 epoch, final int subnetCount) {
