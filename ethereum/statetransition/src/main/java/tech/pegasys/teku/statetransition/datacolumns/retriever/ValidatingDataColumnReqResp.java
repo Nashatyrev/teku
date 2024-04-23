19cb65ab2f6cd65982ba1bfa1c954dd3f09e4780
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2024-04-23 11:52:40 +0400

    Spotless

--- ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/datacolumns/retriever/ValidatingDataColumnReqResp.java
+++ ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/datacolumns/retriever/ValidatingDataColumnReqResp.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright Consensys Software Inc., 2024
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 package tech.pegasys.teku.statetransition.datacolumns.retriever;
 
 import org.apache.tuweni.units.bigints.UInt256;
@@ -22,7 +35,8 @@ public class ValidatingDataColumnReqResp implements DataColumnReqResp {
   }
 
   @Override
-  public SafeFuture<DataColumnSidecar> requestDataColumnSidecar(UInt256 nodeId, DataColumnIdentifier columnIdentifier) {
+  public SafeFuture<DataColumnSidecar> requestDataColumnSidecar(
+      UInt256 nodeId, DataColumnIdentifier columnIdentifier) {
     return reqResp
         .requestDataColumnSidecar(nodeId, columnIdentifier)
         .thenCompose(
@@ -30,8 +44,7 @@ public class ValidatingDataColumnReqResp implements DataColumnReqResp {
                 validator
                     .validate(sidecar)
                     .thenApply(__ -> sidecar)
-                    .catchAndRethrow(
-                        err -> peerManager.banNode(nodeId)));
+                    .catchAndRethrow(err -> peerManager.banNode(nodeId)));
   }
 
   @Override
