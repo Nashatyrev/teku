e52c9e82f0a7bf3226869bee2315339314574e07
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2024-05-03 21:59:23 +0400

    Integrate DataColumnSidecarCustody for storing gossip inbound/outbound sidecars

--- ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/datacolumns/DataColumnSidecarCustodyImpl.java
+++ ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/datacolumns/DataColumnSidecarCustodyImpl.java
@@ -27,12 +27,11 @@ import tech.pegasys.teku.infrastructure.unsigned.UInt64;
 import tech.pegasys.teku.spec.Spec;
 import tech.pegasys.teku.spec.SpecMilestone;
 import tech.pegasys.teku.spec.datastructures.blobs.versions.eip7594.DataColumnSidecar;
-import tech.pegasys.teku.spec.datastructures.blocks.SignedBeaconBlock;
 import tech.pegasys.teku.spec.datastructures.networking.libp2p.rpc.DataColumnIdentifier;
 import tech.pegasys.teku.spec.logic.versions.eip7594.helpers.MiscHelpersEip7594;
-import tech.pegasys.teku.storage.client.CombinedChainDataClient;
 
-public class DataColumnSidecarCustodyImpl implements DataColumnSidecarCustody, SlotEventsChannel {
+public class DataColumnSidecarCustodyImpl
+    implements UpdatableDataColumnSidecarCustody, SlotEventsChannel {
 
   public interface BlockChainAccessor {
 
@@ -81,20 +80,14 @@ public class DataColumnSidecarCustodyImpl implements DataColumnSidecarCustody, S
 
   public DataColumnSidecarCustodyImpl(
       Spec spec,
-      CombinedChainDataClient combinedChainDataClient,
-      DataColumnSidecarDB db,
       BlockChainAccessor blockChainAccessor,
+      DataColumnSidecarDB db,
       UInt256 nodeId,
       int totalCustodySubnetCount) {
     this.spec = spec;
     this.db = db;
     // FIXME: I stink!
-    this.blockChainAccessor =
-        slot ->
-            combinedChainDataClient
-                .getBlockAtSlotExact(slot)
-                .thenApply(maybeBlock -> maybeBlock.map(SignedBeaconBlock::getRoot))
-                .join();
+    this.blockChainAccessor = blockChainAccessor;
     this.nodeId = nodeId;
     this.totalCustodySubnetCount = totalCustodySubnetCount;
     this.eip7594StartEpoch = spec.getForkSchedule().getFork(SpecMilestone.EIP7594).getEpoch();
