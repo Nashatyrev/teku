fbe7f7c009343057bca560b744460e230c162e2a
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2024-05-08 20:16:30 +0400

    Align computeDataColumnSidecarBackboneSubnets with current spec

--- ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/datacolumns/retriever/SimpleSidecarRetriever.java
+++ ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/datacolumns/retriever/SimpleSidecarRetriever.java
@@ -201,7 +201,7 @@ public class SimpleSidecarRetriever
       //      this.extraCustodySubnetCount = extraCustodySubnetCount;
     }
 
-    private Set<UInt64> getNodeCustodyIndexes(UInt64 slot) {
+    private List<UInt64> getNodeCustodyIndexes(UInt64 slot) {
       SpecVersion specVersion = spec.atSlot(slot);
       //      int minCustodyRequirement =
       //          SpecConfigEip7594.required(specVersion.getConfig()).getCustodyRequirement();
