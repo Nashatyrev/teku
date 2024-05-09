45bd7128718b26838931d242e9cdddc88e19a876
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2024-05-09 15:19:31 +0400

    Change ERR debug output to LOG

--- ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/datacolumns/DasCustodySync.java
+++ ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/datacolumns/DasCustodySync.java
@@ -105,24 +105,21 @@ public class DasCustodySync implements SlotEventsChannel {
           err -> onRequestException(request, err));
     }
 
-
     if (missingColumnsToRequest.isEmpty()) {
       coolDownTillNextSlot = true;
     }
 
-    { // TODO remove debug println
+    {
       Set<UInt64> missingSlots =
           missingColumnsToRequest.stream()
               .map(ColumnSlotAndIdentifier::slot)
               .collect(Collectors.toSet());
-      final String slotsString = missingSlots.toString();
-      System.err.println(
-          String.format(
-              "DataCustodySync.fillUp: synced=%s pending=%s, missingColumns=%s(%s)",
-              syncedColumnCount,
-              pendingRequests.size(),
-              missingColumnsToRequest.size(),
-              slotsString));
+      LOG.debug(
+          "DataCustodySync.fillUp: synced={} pending={}, missingColumns={}({})",
+          syncedColumnCount,
+          pendingRequests.size(),
+          missingColumnsToRequest.size(),
+          missingSlots);
     }
   }
 
