06cdac236fc234fe31495c56b092ea39df32fd52
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2024-06-05 13:07:43 +0400

    Remove Subcomponents for now

--- networking/p2p/src/main/java/tech/pegasys/teku/networking/p2p/discovery/DiscoveryNetworkDaggerSubcomponent.java
+++
@@ -1,9 +0,0 @@
-package tech.pegasys.teku.networking.p2p.discovery;
-
-import dagger.Subcomponent;
-
-@Subcomponent(modules = DiscoveryNetworkDaggerModule.class)
-public interface DiscoveryNetworkDaggerSubcomponent {
-
-  DiscoveryNetwork<?> discoveryNetwork();
-}
