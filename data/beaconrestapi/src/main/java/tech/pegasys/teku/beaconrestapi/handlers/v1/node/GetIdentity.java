1922c3bf40638c7f4e382325ea008d8beecae536
Dmitrii Shmatko <zilm@zilm.net>
2024-08-15 19:12:16 +0200

    Add description to custody_subnet_count field

--- data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/handlers/v1/node/GetIdentity.java
+++ data/beaconrestapi/src/main/java/tech/pegasys/teku/beaconrestapi/handlers/v1/node/GetIdentity.java
@@ -140,7 +140,9 @@ public class GetIdentity extends RestApiEndpoint {
                     "Bitvector representing the node's persistent sync committee subnet subscriptions."),
             MetadataMessage::getOptionalSyncnets)
         .withOptionalField(
-            "custody_subnet_count", UINT64_TYPE, MetadataMessage::getOptionalCustodySubnetCount)
+            "custody_subnet_count",
+            UINT64_TYPE.withDescription("PeerDAS custody subnet count."),
+            MetadataMessage::getOptionalCustodySubnetCount)
         .build();
   }
 
