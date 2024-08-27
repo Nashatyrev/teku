4239454238c46e71a1a604db765c34f2c3131e51
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2024-08-27 14:19:43 +0400

    Implement minimal RX functionality

---
+++ ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/datacolumns/util/rx/AsyncIteratorCollector.java
@@ -0,0 +1,39 @@
+package tech.pegasys.teku.statetransition.datacolumns.util.rx;
+
+import tech.pegasys.teku.infrastructure.async.SafeFuture;
+
+import java.util.Collection;
+
+public class AsyncIteratorCollector<T> {
+
+  private final AsyncIterator<T> iterator;
+
+  public AsyncIteratorCollector(AsyncIterator<T> iterator) {
+    this.iterator = iterator;
+  }
+
+  public <C extends Collection<T>> SafeFuture<C> collect(C collection) {
+    SafeFuture<C> promise = new SafeFuture<>();
+    iterator.iterate(
+        new AsyncIteratorCallback<T>() {
+          @Override
+          public boolean onNext(T t) {
+            synchronized (collection) {
+              collection.add(t);
+            }
+            return true;
+          }
+
+          @Override
+          public void onComplete() {
+            promise.complete(collection);
+          }
+
+          @Override
+          public void onError(Throwable t) {
+            promise.completeExceptionally(t);
+          }
+        });
+    return promise;
+  }
+}
---
+++ ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/datacolumns/util/rx/LimitIteratorCallback.java
@@ -0,0 +1,23 @@
+package tech.pegasys.teku.statetransition.datacolumns.util.rx;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class LimitIteratorCallback<T> extends AbstractDelegatingIteratorCallback<T, T> {
+
+  private final AtomicLong limit;
+
+
+  protected LimitIteratorCallback(AsyncIteratorCallback<T> delegate, long limit) {
+    super(delegate);
+    this.limit = new AtomicLong(limit);
+  }
+
+  @Override
+  public boolean onNext(T t) {
+    if (limit.getAndDecrement() > 0) {
+      return delegate.onNext(t);
+    } else {
+      return false;
+    }
+  }
+}
