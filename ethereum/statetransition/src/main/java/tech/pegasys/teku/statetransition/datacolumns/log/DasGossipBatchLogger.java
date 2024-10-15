304b087828bf0e3ef4ea3afa01ba4dfd3ef190a3
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2024-10-15 20:02:06 +0400

    Implement DasGossipBatchLogger.

--- ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/datacolumns/log/DasGossipBatchLogger.java
+++ ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/datacolumns/log/DasGossipBatchLogger.java
@@ -4,47 +4,59 @@ import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
+import java.util.Optional;
+import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import tech.pegasys.teku.infrastructure.async.AsyncRunner;
 import tech.pegasys.teku.infrastructure.logging.LogFormatter;
 import tech.pegasys.teku.infrastructure.time.TimeProvider;
 import tech.pegasys.teku.spec.datastructures.blobs.versions.eip7594.DataColumnSidecar;
 import tech.pegasys.teku.spec.datastructures.blocks.SlotAndBlockRoot;
-import tech.pegasys.teku.spec.datastructures.util.DataColumnSlotAndIdentifier;
 import tech.pegasys.teku.statetransition.datacolumns.util.StringifyUtil;
 import tech.pegasys.teku.statetransition.validation.InternalValidationResult;
 import tech.pegasys.teku.statetransition.validation.ValidationResultCode;
 
 public class DasGossipBatchLogger implements DasGossipLogger {
-  private final Logger logger;
+  private static final Logger LOG = LogManager.getLogger(DasGossipLogger.class);
   private final TimeProvider timeProvider;
 
   private List<Event> events = new ArrayList<>();
 
-  public DasGossipBatchLogger(
-      AsyncRunner asyncRunner, TimeProvider timeProvider, Logger logger) {
+  public DasGossipBatchLogger(AsyncRunner asyncRunner, TimeProvider timeProvider) {
     this.timeProvider = timeProvider;
-    this.logger = logger;
     asyncRunner.runWithFixedDelay(
-        this::sample,
+        this::logBatchedEvents,
         Duration.ofSeconds(1),
-        err -> logger.debug("DasGossipBatchLogger error: " + err));
+        err -> LOG.debug("DasGossipBatchLogger error: {}", err.toString()));
   }
 
   interface Event {
+
     long time();
   }
 
-  record GossipReceiveEvent(long time, DataColumnSidecar sidecar) implements Event {}
+  interface ColumnEvent extends Event {
+    DataColumnSidecar sidecar();
+  }
 
-  record GossipValidateEvent(
+  record ReceiveEvent(
       long time, DataColumnSidecar sidecar, InternalValidationResult validationResult)
-      implements Event {}
+      implements ColumnEvent {}
 
-  private void sample() {
+  record PublishEvent(long time, DataColumnSidecar sidecar, Optional<Throwable> result)
+      implements ColumnEvent {}
+
+  record SubscribeEvent(long time, int subnetId) implements Event {}
+
+  record UnsubscribeEvent(long time, int subnetId) implements Event {}
+
+  private void logBatchedEvents() {
     final List<Event> eventsLoc;
     synchronized (this) {
       if (events.isEmpty()) {
@@ -54,76 +66,147 @@ public class DasGossipBatchLogger implements DasGossipLogger {
       events = new ArrayList<>();
     }
 
-    Map<SlotAndBlockRoot, List<GossipValidateEvent>> eventsByBlock = new TreeMap<>();
-    for (Event event : eventsLoc) {
-      if (event instanceof GossipValidateEvent e) {
+    groupByBlock(ReceiveEvent.class, eventsLoc).forEach(this::logReceiveEvents);
+    groupByBlock(PublishEvent.class, eventsLoc).forEach(this::logPublishEvents);
+    logSubscriptionEvents(eventsLoc);
+  }
+
+  private void logReceiveEvents(SlotAndBlockRoot blockId, List<ReceiveEvent> events) {
+    Map<ValidationResultCode, List<ReceiveEvent>> eventsByValidateCode =
+        events.stream().collect(Collectors.groupingBy(e -> e.validationResult().code()));
+    eventsByValidateCode.forEach(
+        (validationCode, codeEvents) -> {
+          Level level = validationCode == ValidationResultCode.REJECT ? Level.INFO : Level.DEBUG;
+          LOG.log(
+              level,
+              "Received {} data columns (validation result: {}) by gossip {} for block {}: {}",
+              codeEvents.size(),
+              validationCode,
+              msAgoString(codeEvents),
+              blockIdString(blockId),
+              columnIndexesString(codeEvents));
+        });
+  }
+
+  private void logPublishEvents(SlotAndBlockRoot blockId, List<PublishEvent> events) {
+    Map<Optional<Class<?>>, List<PublishEvent>> eventsByError =
+        events.stream()
+            .collect(
+                Collectors.groupingBy(
+                    e -> e.result().map(thr -> ExceptionUtils.getRootCause(thr).getClass())));
+    eventsByError.forEach(
+        (maybeErrorClass, errEvents) -> {
+          Optional<Throwable> someError = errEvents.getFirst().result();
+          someError.ifPresentOrElse(
+              error -> {
+                LOG.info(
+                    "Error publishing {} data columns ({}) by gossip {} for block {}: {}",
+                    errEvents.size(),
+                    columnIndexesString(errEvents),
+                    msAgoString(errEvents),
+                    blockIdString(blockId),
+                    error);
+              },
+              () -> {
+                LOG.debug(
+                    "Published {} data columns by gossip {} for block {}: {}",
+                    errEvents.size(),
+                    msAgoString(errEvents),
+                    blockIdString(blockId),
+                    columnIndexesString(errEvents));
+              });
+        });
+  }
+
+  private void logSubscriptionEvents(List<Event> events) {
+    List<Integer> subscribedSubnets = new ArrayList<>();
+    List<Integer> unsubscribedSubnets = new ArrayList<>();
+    events.forEach(
+        e -> {
+          switch (e) {
+            case SubscribeEvent event -> subscribedSubnets.add(event.subnetId());
+            case UnsubscribeEvent event -> unsubscribedSubnets.add(event.subnetId());
+            default -> {}
+          }
+        });
+
+    if (!(subscribedSubnets.isEmpty() && unsubscribedSubnets.isEmpty())) {
+      String subscribeString =
+          subscribedSubnets.isEmpty()
+              ? ""
+              : "subscribed: " + StringifyUtil.toIntRangeStringWithSize(subscribedSubnets);
+      String unsubscribeString =
+          unsubscribedSubnets.isEmpty()
+              ? ""
+              : "unsubscribed: " + StringifyUtil.toIntRangeStringWithSize(unsubscribedSubnets);
+      String maybeDelim = subscribedSubnets.isEmpty() || unsubscribedSubnets.isEmpty() ? "" : ", ";
+      LOG.info(
+          "Data column gossip subnets subscriptions changed: "
+              + subscribeString
+              + maybeDelim
+              + unsubscribeString);
+    }
+  }
+
+  private String columnIndexesString(List<? extends ColumnEvent> events) {
+    List<Integer> columnIndexes =
+        events.stream().map(e -> e.sidecar().getIndex().intValue()).toList();
+    return StringifyUtil.toIntRangeString(columnIndexes);
+  }
+
+  private String blockIdString(SlotAndBlockRoot blockId) {
+    return "#"
+        + blockId.getSlot()
+        + " (0x"
+        + LogFormatter.formatAbbreviatedHashRoot(blockId.getBlockRoot())
+        + ")";
+  }
+
+  private String msAgoString(List<? extends Event> events) {
+    long curTime = timeProvider.getTimeInMillis().longValue();
+    long firstMillisAgo = curTime - events.getFirst().time();
+    long lastMillisAgo = curTime - events.getLast().time();
+    return (lastMillisAgo == firstMillisAgo
+            ? lastMillisAgo + "ms"
+            : lastMillisAgo + "ms-" + firstMillisAgo + "ms")
+        + " ago";
+  }
+
+  @Override
+  public synchronized void onReceive(
+      DataColumnSidecar sidecar, InternalValidationResult validationResult) {
+    events.add(
+        new ReceiveEvent(timeProvider.getTimeInMillis().longValue(), sidecar, validationResult));
+  }
+
+  @Override
+  public synchronized void onPublish(DataColumnSidecar sidecar, Optional<Throwable> result) {
+    events.add(new PublishEvent(timeProvider.getTimeInMillis().longValue(), sidecar, result));
+  }
+
+  @Override
+  public void onDataColumnSubnetSubscribe(int subnetId) {
+    events.add(new SubscribeEvent(timeProvider.getTimeInMillis().longValue(), subnetId));
+  }
+
+  @Override
+  public void onDataColumnSubnetUnsubscribe(int subnetId) {
+    events.add(new UnsubscribeEvent(timeProvider.getTimeInMillis().longValue(), subnetId));
+  }
+
+  private static <TEvent extends ColumnEvent>
+      SortedMap<SlotAndBlockRoot, List<TEvent>> groupByBlock(
+          Class<TEvent> eventClass, List<Event> allEvents) {
+    SortedMap<SlotAndBlockRoot, List<TEvent>> eventsByBlock = new TreeMap<>();
+    for (Event event : allEvents) {
+      if (eventClass.isAssignableFrom(event.getClass())) {
+        @SuppressWarnings("unchecked")
+        TEvent e = (TEvent) event;
         DataColumnSidecar sidecar = e.sidecar();
         SlotAndBlockRoot blockId = new SlotAndBlockRoot(sidecar.getSlot(), sidecar.getBlockRoot());
         eventsByBlock.computeIfAbsent(blockId, __ -> new ArrayList<>()).add(e);
       }
     }
-    eventsByBlock.forEach(this::logGossipForBlock);
-  }
-
-  private void logGossipForBlock(SlotAndBlockRoot blockId, List<GossipValidateEvent> events) {
-    long curTime = timeProvider.getTimeInMillis().longValue();
-    Set<ValidationResultCode> validationCodes =
-        events.stream().map(e -> e.validationResult().code()).collect(Collectors.toSet());
-    if (validationCodes.size() == 1) {
-      ValidationResultCode validationCode = events.getFirst().validationResult().code();
-      List<Integer> columnIndexes =
-          events.stream().map(e -> e.sidecar().getIndex().intValue()).toList();
-      long firstMillisAgo = curTime - events.getFirst().time();
-      long lastMillisAgo = curTime - events.getLast().time();
-      String timeStr = " (" + lastMillisAgo + " - " + firstMillisAgo + ") ms ago";
-      String blockStr =
-          blockId.getSlot()
-              + " ("
-              + LogFormatter.formatAbbreviatedHashRoot(blockId.getBlockRoot())
-              + ")";
-      String s =
-          "Received "
-              + events.size()
-              + " data columns (validation result: " + validationCode + ") by gossip "
-              + timeStr
-              + " for block "
-              + blockStr
-              + ": "
-              + StringifyUtil.toIntRangeString(columnIndexes);
-      logger.debug(s);
-    } else {
-      events.forEach(
-          e -> {
-            String timeStr = " " + (curTime - e.time()) + " ms ago";
-            String s =
-                "Received "
-                    + e.validationResult().code()
-                    + " data column by gossip "
-                    + timeStr
-                    + ": "
-                    + DataColumnSlotAndIdentifier.fromDataColumn(e.sidecar());
-          });
-    }
-  }
-
-  @Override
-  public synchronized void onGossipInboundReceive(DataColumnSidecar sidecar) {
-    events.add(new GossipReceiveEvent(timeProvider.getTimeInMillis().longValue(), sidecar));
-  }
-
-  @Override
-  public synchronized void onGossipInboundValidate(
-      DataColumnSidecar sidecar, InternalValidationResult validationResult) {
-    events.add(
-        new GossipValidateEvent(
-            timeProvider.getTimeInMillis().longValue(), sidecar, validationResult));
-  }
-
-  @Override
-  public synchronized void onGossipOutboundPublish(DataColumnSidecar sidecar) {}
-
-  @Override
-  public void onGossipOutboundPublishError(DataColumnSidecar sidecar, Throwable err) {
-
+    return eventsByBlock;
   }
 }
