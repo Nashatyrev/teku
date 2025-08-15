2c94a218ccf140ab0030fac99e65e1f1a145b263
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2025-08-15 20:04:23 +0200

    Fix V6Schema: add new variables to the all list

--- storage/src/main/java/tech/pegasys/teku/storage/server/kvstore/schema/V6SchemaCombined.java
+++ storage/src/main/java/tech/pegasys/teku/storage/server/kvstore/schema/V6SchemaCombined.java
@@ -285,6 +285,10 @@ public abstract class V6SchemaCombined implements SchemaCombined {
         .put("EARLIEST_BLOB_SIDECAR_SLOT", getVariableEarliestBlobSidecarSlot())
         .put("EARLIEST_BLOCK_SLOT_AVAILABLE", getVariableEarliestBlockSlot())
         .put("LATEST_CANONICAL_BLOCK_ROOT", getVariableLatestCanonicalBlockRoot())
+        .put("CONFIRMED_ROOT", getVariableConfirmedRoot())
+        .put("PREV_SLOT_JUSTIFIED_CHECKPOINT", getVariablePrevSlotJustifiedCheckpoint())
+        .put("PREV_SLOT_UNREALIZED_JUSTIFIED_CHECKPOINT", getVariablePrevSlotUnrealizedJustifiedCheckpoint())
+        .put("PREV_SLOT_HEAD", getVariablePrevSlotHead())
         .put("FIRST_CUSTODY_INCOMPLETE_SLOT", getVariableFirstCustodyIncompleteSlot())
         .put("FIRST_SAMPLER_INCOMPLETE_SLOT", getVariableFirstSamplerIncompleteSlot())
         .build();
