bfd3d0243395a48aeeeb03bdef83e51fde20be25
Anton Nashatyrev <anton.nashatyrev@gmail.com>
2025-09-24 17:15:27 +0400

    ForkChoice: Return back calling processHead() on block reorgs only (not on every block). When running Teku the processHead() is invoked by the ForkChoiceRatchet on slot start. In ForkChoice or ConfirmationRule unit tests it should be called manually

--- ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/forkchoice/ForkChoice.java
+++ ethereum/statetransition/src/main/java/tech/pegasys/teku/statetransition/forkchoice/ForkChoice.java
@@ -885,20 +885,20 @@ public class ForkChoice implements ForkChoiceUpdatedResultSubscriber {
 
     // FIXME we probably shouldn't call processHead() every block
     // however may leav in prototype if it's just a matter of performance
-    //    if (!result.isBlockOnCanonicalChain() && shouldApplyProposerBoost) {
-    // This is likely a reorging block that requires a full processHead to update the head.
-    // Running processHead here will ensure:
-    //
-    // - node producing a reorging block will attest and produce sync committee for the reorging
-    // block
-    // - node just importing (not producing it) a reorging block will produce sync committee for
-    // the new head
-    //
-    // It wouldn't be enough to trigger processHead at attestationDue because it will only
-    // cover attestation generation and not sync committee message, since it is generated
-    // VC side and requires head event to be sent before attestationDue.
-    processHead().finish(error -> LOG.error("Fork choice updating head failed", error));
-    //    }
+    if (!result.isBlockOnCanonicalChain() && shouldApplyProposerBoost) {
+      // This is likely a reorging block that requires a full processHead to update the head.
+      // Running processHead here will ensure:
+      //
+      // - node producing a reorging block will attest and produce sync committee for the reorging
+      // block
+      // - node just importing (not producing it) a reorging block will produce sync committee for
+      // the new head
+      //
+      // It wouldn't be enough to trigger processHead at attestationDue because it will only
+      // cover attestation generation and not sync committee message, since it is generated
+      // VC side and requires head event to be sent before attestationDue.
+      processHead().finish(error -> LOG.error("Fork choice updating head failed", error));
+    }
   }
 
   private SlotAndBlockRoot findNewChainHead(final ForkChoiceStrategy forkChoiceStrategy) {
